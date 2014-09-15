ae99050de53a78ee25e2e3dd5c176f2b5465d34a
Ben West <bewest@gmail.com>
2014-09-15 13:52:15 -0700

    install drywall-compile in ./node_modules/.bin
    Add a script to build the tools.
    The tool should be run from root of repo like this:
    $ ./node_modules/.bin/drywall-compile
    
    The compells drywall to download assets needed via bower, then builds
    the assets into the public subdirectory using grunt.
    I was unable to find a way to get grunt to work during npm install
    process, or as part of the "postinstall" script.  However, running the
    script once it has been installed from root of repo does seem to work.
    
    There are warnings on my system for things like "grunt-contrib-copy"
    not found, is it installed on the system, so I've left in some
    debugging I attempted to try to get these warnings to go away.
    Despite the warnings, none of my attempts (shown in comments) worked
    to eliminate the messages, and yet the build seems to produce correct
    results.

--- build.js
+++ build.js
@@ -1,13 +1,27 @@
+#!/usr/bin/env node
 
 function build ( ) {
   var grunt = require('grunt');
+  // grunt.loadNpmTasks('grunt-contrib-copy');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-less');
+  // grunt.loadNpmTasks('grunt-newer');
+  var path = require('path');
+  console.log("DIR", __dirname);
+  var orig_dir = process.cwd( );
   process.cwd(__dirname);
-  var config = require('./Gruntfile')
-  config(grunt);
   var bower = require('bower');
   bower.commands.install( )
   .on('end', function (installed) {
+    process.cwd(orig_dir);
+    var file = path.resolve(__dirname, 'Gruntfile');
+    var config = require(file)
+    config(grunt);
+    grunt.file.setBase(path.resolve(__dirname));
+    console.log(process.env.FORCE_BUILD);
+    console.log('grunt running');
     grunt.tasks(['build']);
+    console.log('grunt finished');
   });
 
 }
